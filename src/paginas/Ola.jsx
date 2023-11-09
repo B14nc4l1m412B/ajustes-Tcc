94519ebc6ca1fce4bf15830b43b1d7f17d688d25
AAAA <AAAA@gmail.com>
2023-11-09 14:30:54 -0300

    ajustes 2

--- src/paginas/Ola.jsx
+++ src/paginas/Ola.jsx
@@ -30,7 +30,7 @@ function Ola() {
           <img className={styles.imagemdefundo} src={imagemdefundo} alt="" />
           <div className={styles.conteudo}>
             <h1 className={styles.h1}>Olá</h1>
-            <p className={styles.p}>Bem-vindo ao site Begin DoorLock systems, onde você encontra uma grande variedade de sirenes, equipamentos de segurança residencial, tudo na melhor qualidade para sua segurança e proteção residencial, confira nossos equipamentos de última geração.</p>
+            <h2 className={styles.h2}>Bem-vindo ao site Begin DoorLock systems, onde você encontra uma grande variedade de sirenes, equipamentos de segurança residencial, tudo na melhor qualidade para sua segurança e proteção residencial, confira nossos equipamentos de última geração.</h2>
           </div>
         </main>
       </>
